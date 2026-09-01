30042aafd314af69905a7081c57b86138d8c8beb
Ruthvika <ruthvika2468@gmail.com>
2026-09-01 18:33:54 +0530

    Add WASD blob movement

--- Assets/scripts/Blob Movement.cs
+++ Assets/scripts/Blob Movement.cs
@@ -3,19 +3,24 @@ using UnityEngine.InputSystem;
 
 public class BlobMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current != null && Keyboard.current.wKey.isPressed)
-        {
-            transform.Translate(Vector3.forward * 5f * Time.deltaTime);
-        }
+        Vector3 movement = Vector3.zero;
 
+        if (Keyboard.current.wKey.isPressed)
+            movement += Vector3.forward;
+
+        if (Keyboard.current.sKey.isPressed)
+            movement += Vector3.back;
+
+        if (Keyboard.current.aKey.isPressed)
+            movement += Vector3.left;
+
+        if (Keyboard.current.dKey.isPressed)
+            movement += Vector3.right;
+
+        transform.Translate(movement.normalized * speed * Time.deltaTime);
     }
 }
